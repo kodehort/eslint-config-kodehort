ce288bcc66a42fa69e2c6eb007ca219017c93547
alistairstead <alistair_stead@me.com>
2024-04-10 21:39:09 +0100

    feat(eslint-config): update eslint config and dependencies
    This commit includes several changes to the eslint configuration and
    dependencies. The changes include updates to the eslint config files,
    updates to the package.json file to reflect the updated dependencies,
    and changes to the typescript configuration. The commit also includes
    some minor code formatting changes.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import { baseConfig } from './dist/index.js'
+import { baseConfig } from "./dist/index.js";
 
-export default [...baseConfig, {
-  languageOptions: {
-    parserOptions: {
-      project: [
-        './tsconfig.json',
-        './tsconfig.eslint.json',
-      ],
+export default [
+  ...baseConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.eslint.json"],
+      },
     },
-  }
-}]
+  },
+];
--- src/base.ts
+++ src/base.ts
@@ -1,49 +1,55 @@
-import type { Linter } from 'eslint'
+import type { Linter } from "eslint";
 
-import { astro } from './config/astro.js'
-import { browser } from './config/browser.js'
-import { eslint } from './config/eslint.js'
-import { jsdoc } from './config/jsdoc.js'
-import { json } from './config/json.js'
-import { node } from './config/node.js'
-import { onlyWarn } from './config/only-warn.js'
-import { perfectionist } from './config/perfectionist.js'
-import { prettierThisMustBePutLast } from './config/prettier.js'
-import { regex } from './config/regex.js'
-import { security } from './config/security.js'
-import { solid } from './config/solid.js'
-import { turbo } from './config/turbo.js'
-import { typescript } from './config/typescript.js'
-import { unicorn } from './config/unicorn.js'
-import { vitest } from './config/vitest.js'
-import { yaml } from './config/yaml.js'
-import { inferPackageTypes } from './util/infer-package-type.js'
-import { unignore } from './util/unignore.js'
-import { imports } from './config/import.js'
-import { comment } from './config/no-commented-code.js'
-import { arrowFunctions } from './config/arrow-functions.js'
+import { astro } from "./config/astro.js";
+import { browser } from "./config/browser.js";
+import { eslint } from "./config/eslint.js";
+import { jsdoc } from "./config/jsdoc.js";
+import { json } from "./config/json.js";
+import { node } from "./config/node.js";
+import { onlyWarn } from "./config/only-warn.js";
+import { perfectionist } from "./config/perfectionist.js";
+import { prettierThisMustBePutLast } from "./config/prettier.js";
+import { regex } from "./config/regex.js";
+import { security } from "./config/security.js";
+import { solid } from "./config/solid.js";
+import { turbo } from "./config/turbo.js";
+import { typescript } from "./config/typescript.js";
+import { unicorn } from "./config/unicorn.js";
+import { vitest } from "./config/vitest.js";
+import { yaml } from "./config/yaml.js";
+import { inferPackageTypes } from "./util/infer-package-type.js";
+// import { unignore } from "./util/unignore.js";
+import { imports } from "./config/import.js";
+import { comment } from "./config/no-commented-code.js";
+import { arrowFunctions } from "./config/arrow-functions.js";
 
-const rootFiles = ['*.js', '*.cjs', '*.mjs', '*.ts']
-const { browserPackages, nodePackages } = await inferPackageTypes()
-const browserFiles = browserPackages.map((dir) => `${dir}/**/*.{ts,tsx}`)
-const nodeFiles = nodePackages.map((dir) => `${dir}/**/*.ts`)
-const astroFiles = browserPackages.map((dir) => `${dir}/**/*.{astro}`)
-const testFiles = ['tests/**', '*.test.ts?(x)', '*.spec.ts?(x)']
+const rootFiles = ["*.js", "*.cjs", "*.mjs", "*.ts", "*.yaml", "*.json"];
+const { browserPackages, nodePackages } = await inferPackageTypes();
+
+const browserFiles = browserPackages.map((dir) => `${dir}/**/*.{ts,tsx}`);
+const nodeFiles = nodePackages.map((dir) => `${dir}/**/*.ts`);
+const astroFiles = browserPackages.map((dir) => `${dir}/**/*.{astro}`);
+const testFiles = ["*.test.ts?(x)", "*.spec.ts?(x)"];
 const allFiles = [
   ...rootFiles,
   ...browserFiles,
   ...astroFiles,
   ...nodeFiles,
   ...testFiles,
-]
+];
 
-export const baseConfig : Linter.FlatConfig[] = [
+function test() {}
+
+test();
+
+export const baseConfig: Linter.FlatConfig[] = [
   {
     ignores: [
-      '**/*.d.ts',
-      'node_modules/**',
-      'pnpm-lock.yaml',
-      ...unignore(rootFiles),
+      "**/*.d.ts",
+      "node_modules/**",
+      "pnpm-lock.yaml",
+      "package.json",
+      // ...unignore(rootFiles),
     ],
   },
   ...browser({ files: [...browserFiles] }),
@@ -66,4 +72,4 @@ export const baseConfig : Linter.FlatConfig[] = [
   ...onlyWarn(),
   ...turbo({ files: allFiles }),
   ...prettierThisMustBePutLast(),
-]
+];
